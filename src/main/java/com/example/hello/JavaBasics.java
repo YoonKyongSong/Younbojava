4b98bcd2b4373ae0d2c362b793a8eba099900f25
Yoon <20231377@sungshin.ac,kr>
2024-05-03 16:31:43 +0900

    융보공자바암호학 AES 커밋

--- src/main/java/com/example/hello/JavaBasics.java
+++ src/main/java/com/example/hello/JavaBasics.java
@@ -1,5 +1,12 @@
 package com.example.hello;
 
-public class JavaBasics {
-
+class MyClass {
+public static void main(String[] args) {
+int arr[][] = { {100,200}, {300,400} };
+for (int i = 0; i < arr.length; i++) {
+for (int j = 0; j < arr.length; j++) {
+System.out.println(arr[i][j]);
+}
+}
+}
 }
